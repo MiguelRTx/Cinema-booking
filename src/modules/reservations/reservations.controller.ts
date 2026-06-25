40f648aa1f9a43996815444f0e95393ef4a60763
MAMADUS <saldiaspro2005@gmail.com>
2026-06-25 13:55:52 -0400

    Correccion en controller de reservation, ahora el admin no puede reservar solo CLIENTES

--- src/modules/reservations/reservations.controller.ts
+++ src/modules/reservations/reservations.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Post, Get, Body, UseGuards, Request } from '@nestjs/common'
 import { ReservationsService } from './reservations.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { JwtAuthGuard } from '../../common/guards/jwt-auth/jwt-auth.guard';
+import { RolesGuard } from '../../common/guards/roles/roles.guard';
+import { Roles } from '../../common/decorators/roles.decorator';
 
 interface AuthenticatedRequest extends Request {
   user: { id: string; email: string; role: string };
@@ -12,7 +14,8 @@ export class ReservationsController {
   constructor(private readonly reservationsService: ReservationsService) {}
 
   @Post()
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('CLIENT')
   create(
     @Request() req: AuthenticatedRequest,
     @Body() createReservationDto: CreateReservationDto,
@@ -25,6 +28,8 @@ export class ReservationsController {
 
   @Get('me')
   @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('CLIENT')
   findByUser(@Request() req: AuthenticatedRequest) {
     return this.reservationsService.findByUser(req.user.id);
   }
